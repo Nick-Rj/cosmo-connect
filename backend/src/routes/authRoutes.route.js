63092f64c848dcb50e68f97184dc58517b978739
Nikhil Raj <nik.rj10348@gmail.com>
2026-01-18 18:49:42 +0530

    feature: Integrated arcjet for limiting API calls

--- backend/src/routes/authRoutes.route.js
+++ backend/src/routes/authRoutes.route.js
@@ -7,12 +7,22 @@ import {
   updateUserProfile,
 } from '../controllers/auth.controller.js';
 import { authenticateUser } from '../middlewares/auth.middleware.js';
+import { arcjetProtection } from '../middlewares/arcjet.middleware.js';
 
 const authRouter = express.Router();
 
-authRouter.post('/signup', signupController);
-authRouter.post('/login', loginController);
-authRouter.post('/logout', logoutController);
-authRouter.put('/update-profile', authenticateUser, updateUserProfile);
-authRouter.get('/check-user', authenticateUser, acknowledgeUser);
+// Option 1: Passing arcjet middleware directly to the authRouter. First, this middleware will be executed.
+authRouter.use(arcjetProtection);
+
+// Test API for Arcjet: Test with Browser
+authRouter.get('/test-aj', (_, res) => {
+  res.status(200).json({ message: 'Arcjet Test', success: true });
+});
+
+// Option 2: Passing arcjet middleware to all the routes separately.
+authRouter.post('/signup', arcjetProtection, signupController);
+authRouter.post('/login', arcjetProtection, loginController);
+authRouter.post('/logout', arcjetProtection, logoutController);
+authRouter.put('/update-profile', arcjetProtection, authenticateUser, updateUserProfile);
+authRouter.get('/check-user', arcjetProtection, authenticateUser, acknowledgeUser);
 export default authRouter;
